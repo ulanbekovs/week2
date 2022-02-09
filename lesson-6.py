57ec63f86b6da504f244c4953a58caa181232f98
ulanbekovs <aktan.ulanbekov@gmail.com>
2022-02-09 21:03:33 +0600

    this is my first commit number1

---
+++ lesson-6.py
@@ -0,0 +1,68 @@
+# string
+# name = "Nazgul Tabyldieva"
+# # name = name + name[2]
+# print(name)
+# # # print(len(name))
+# # print(name[0:3])  # до 3
+# print(name[0::2]) # nzu через 1
+# print(name[:4:2])
+# print(name[:: -1]) # до конца lugzan
+#
+# #print(dir(name))
+# print(name.count("l", 0, 4))
+# print(name.find("N", 1, 4))
+# print(name.index("z"))
+# print(name.split("z"))
+
+# new_list = name.split()
+#print(new_list)
+
+# name = "Nazgul Tabyldieva"
+# print(name.startswith("N"))
+# print(name.endswith("va"))
+
+#date = input("date: ")
+#name = f"Nazgul Tabyldieva {date}"
+#print(name)
+
+# name = input("name: ")
+# age = input("age: ")
+# work = input("work: ")
+# sensence = f"Меня зовут {name}, мне {age} лет, я работаю в {work}"
+# print(sensence)
+
+#name = input("name: ")
+#age = input("age: ")
+#work = input("work: ")
+#sensence = "Меня зовут {0}, мне {2} лет, я работаю в {1}".format(name, work, age)
+# print(sensence)
+
+
+
+#sen = "и"
+
+#print(sen.isdigit()) #состоит ли строка только из чисел
+#print(sen.isalnum()) #состоит ли строка из чисел и букв
+#print(sen.isalpha()) #состоит ли строка только из букв
+
+'''условные операторы'''
+
+#name = input("name: ")
+#if name == "Nazgul":
+#    print(f"{name} Tabyldieva")
+#elif name == "Nodira":
+#    print(f"{name} Sadyrova")
+#else:
+#    print("not found name")
+
+
+'''practice'''
+
+#num = int(input("Enter a number: "))
+#if num % 2 == 0:
+#    print("even number")
+#else:
+#    print("odd number")
+
+# password = int(input("Enter a password: "))
+
---
+++ lesson-7.py
@@ -0,0 +1,58 @@
+'''списки'''
+
+
+
+#lists = list()
+#print(type(lists))
+#list_1 = []
+#print(type(list_1))
+#list_2 = [10, 12]
+#print(type(list_2))
+
+# lists = [1, 2, "Nodira", "Samat"]
+# lists[-1] = "Ulukbek"
+# print(lists)
+#
+# for i in lists:
+#    print(i)
+
+
+# lists = [1, 2, "Nodira", "Samat"]
+# lists.append(10) # в конце добавляет значение
+# print(lists)
+#
+# black_list = lists.pop(2)
+# print(lists)
+# print(black_list)
+# lists.insert(2, black_list)
+# lists.append(black_list)
+# print(lists)
+
+# str = "1.3.A.f"
+# lists = str.split(".")
+# print(lists)
+
+# fruits = ["banana", "apple", "blueberry"]
+# sentence = " ".join(fruits)   # этот метод передает любой символ в скобках
+# print(sentence)
+
+#student = input("st:")
+#students = ["azamat", "samat", "nodira", "kunduz", "ruslan"]
+#if student in students: # есть еще функция  not in оно дает обратное значение
+#   print("студент уже в групе!!!")
+#else:
+#    print("Зарегистрируйтесь!")
+
+
+# students = ["azamat", "samat", "nodira", "kunduz", "ruslan"]
+# print(students.index("kunduz"))  #print(students.index("kunduz, 2"))
+# print(students.count("azamat")) #считает сколько всего
+# #print(len(students))
+# #print(dir())
+
+#students.extend([10, 29, 17]) # расширяет
+#print(students)
+
+# students.append([10, 29, 17]) # добавит в конец вместе с [10, 29, 17]
+# print(students)
+# print(students[6])
